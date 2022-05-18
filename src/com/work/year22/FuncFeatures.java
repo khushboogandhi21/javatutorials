2a5d392e47e99a83c7e49619f83b9f78ba2be924
Khushboo Gandhi <khushboogandhi21@gmail.com>
2022-05-18 20:07:27 +0530

    latest file

---
+++ src/com/work/year22/FuncFeatures.java
@@ -0,0 +1,24 @@
+package com.work
+
+interface interface_default {
+    void display();
+}
+
+class derived_class{
+
+    public void classMethod(){
+        System.out.println("Derived class Method");
+    }
+}
+
+public class FuncFeatures {
+    public static void main(String[] args){
+
+        derived_class obj1 = new derived_class();
+        interface_default  ref = obj1::classMethod;
+        //we have a reference to the (Functional) interface that references [Not Implements So no polymorphism on LHS]
+        // a class method “classMethod” through obj1 (class object).
+        ref.display(); // so here the implemented method body is called.
+
+    }
+}
---
+++ src/com/work/year22/Optional22Demo.java
@@ -0,0 +1,69 @@
+package com.work.year22;
+
+import java.util.Optional;
+
+public class Optional22Demo {
+
+    public static void main(String[]args){
+
+
+        Optional<Streams22Demo> spliteratorOptional = null;
+        Streams22Demo spliteratorObj = null;
+
+        try {
+            spliteratorOptional = Optional.empty();  //empty
+            System.out.println("empty demo=" + spliteratorOptional.get());  //throws java.util.NoSuchElementException
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        try{
+            spliteratorOptional = Optional.ofNullable(spliteratorObj);  //returns empty if object is null
+            System.out.println("ofNullable demo=" + spliteratorOptional.get());  //throws java.util.NoSuchElementException
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        try{
+            spliteratorOptional = Optional.of(spliteratorObj);  //throws null pointer if obj is null
+            System.out.println("of demo=" + spliteratorOptional.get());  //throws null pointer
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        spliteratorObj = new Streams22Demo();
+        try {
+            spliteratorOptional = Optional.empty();  //empty
+            System.out.println("empty demo=" + spliteratorOptional.get());  //throws java.util.NoSuchElementException
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        try{
+            spliteratorOptional = Optional.ofNullable(spliteratorObj);  //returns empty if object is null
+            System.out.println("ofNullable demo=" + spliteratorOptional.get());  //returns value since its present
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        try{
+            spliteratorOptional = Optional.of(spliteratorObj);  //throws null pointer if obj is null
+            System.out.println("of demo=" + spliteratorOptional.get());  //returns value since its present
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        System.out.println("Is value present = " + spliteratorOptional.isPresent());
+        spliteratorOptional.ifPresent((x) -> {System.out.println("value is Present ="+ x);});  //arg is Consumer Functional Interface [accept(T t) method]
+
+        //arg is Consumer Functional Interface, Runnable Interface that specifies action to perform if empty JAVA 9
+       // spliteratorOptional.ifPresentOrElse((x) -> {System.out.println("value is Present="+ x);}, () -> {System.out.println("value is Empty");});
+
+    }
+
+    //Optional<String> strOpt = Optional.ofNullable("Hello").orElseThrow() ; //orElseThrow(RuntimeException::new);  // DIDNT WORK since final return type is string not optional
+    Optional<String > strOpt = Optional.ofNullable("Hello");
+    String str = strOpt.orElseThrow(RuntimeException::new);
+
+
+
+
+}

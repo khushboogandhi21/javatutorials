89284a347b42f0a71b42b54b196b725fc81f0eb9
Khushboo Gandhi <khushboogandhi21@gmail.com>
2022-10-14 22:34:18 +0530

    Updates 14 Oct 22

--- src/com/leetcode/practice/easy/ValidSudoku.java
+++ src/com/leetcode/practice/easy/ValidSudoku.java
@@ -11,7 +11,6 @@ import java.util.Set;
  * Each column must contain the digits 1-9 without repetition.
  * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
  * Note:
- *
  * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
  * Only the filled cells need to be validated according to the mentioned rules.
  *
